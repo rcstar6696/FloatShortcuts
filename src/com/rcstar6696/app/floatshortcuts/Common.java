1123923f4fe82e4b33e3fd22baaf721f3066e562
Max Weffers <rcstar6696@gmail.com>
2015-09-11 18:02:06 +0200

    Shortcut Creator Release

---
+++ src/com/rcstar6696/app/floatshortcuts/Common.java
@@ -0,0 +1,11 @@
+package com.rcstar6696.app.floatshortcuts;
+
+public class Common {
+
+    public static final int FLAG_FLOATING_WINDOW = 0x00003000;
+
+    public static final String KEY_SYSTEM_SHORTCUT = "system_shortcut";
+    public static final int CODE_REQUEST_SHORTCUT = 100;
+    public static final int CODE_REQUEST_IMAGE = 2;
+
+}
